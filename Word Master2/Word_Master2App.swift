980bfe86f33c527734bb90b9562cd921b672c77c
Mia Patil <miapatil123@gmail.com>
2023-01-20 11:17:58 -0800

    Initial Commit

---
+++ Word Master2/Word_Master2App.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Master2App.swift
+//  Word Master2
+//
+//  Created by Mia Patil on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_Master2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
